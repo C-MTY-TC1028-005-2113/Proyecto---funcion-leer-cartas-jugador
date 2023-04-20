c6fdeedb2a607c6a2a030f8720c7ee5f014ce4e1
roque63 <roque@itesm.mx>
2023-04-20 17:23:14 -0600

    Update exercise.py

--- assignments/04TiendaSillas/src/exercise.py
+++ assignments/04TiendaSillas/src/exercise.py
@@ -14,42 +14,13 @@ def leer_cartas_jugador(nombre, tablero):
 def  despliega_tablero(opcion, matriz, r1 = None, c1 = None, r2 = None, c2 = None):
     pass
 
-def son_pares(opcion, tablero, escondidas, r1, c1, r2, c2):
-    # destapar las cartas en el tablero
-    # desplegar_tablero resaltando las cartas seleccionadas
-    # verificar si son pares
-    # si si son pares asignar a la variable puntos = 1
-    #     desplegar "Par"
-    # si no son pares asignar a  puntos = 0
-    #     desplegar "Impar"
-    #     volver a tapar las cartas en el tablero
-    pass
+
 
 def main() :
     # 1º Leer los datos de entrada
     caso = int(input())
-    if caso == 1:
-        pares_jugador1 = 0
-        escondidas = [['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q'], ['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q']]
-        tablero = [['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]
-        r1,c1,r2,c2 = leer_cartas_jugador("Jugador1", tablero);
-        pares_jugador1 += son_pares(1,tablero, escondidas,r1,c1,r2,c2)
-        print("Pares Jugador1:",pares_jugador1)
-    elif caso == 2:
-        pares_jugador1 = 10
-        escondidas = [['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q'], ['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q']]
-        tablero = [['a', 'b', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', 'b', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]
-        r1,c1,r2,c2 = leer_cartas_jugador("Jugador2", tablero);
-        pares_jugador1 += son_pares(1,tablero, escondidas,r1,c1,r2,c2)
-        print("Pares Jugador2:",pares_jugador1)
-    elif caso == 3:
-        pares_jugador1 = 5
-        escondidas = [['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q'], ['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q']]
-        tablero = [['a', 'b', '?', '?', '?', 'f'], ['g', 'h', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['a', 'b', '?', '?', '?', 'f'], ['g', 'h', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]
-        r1,c1,r2,c2 = leer_cartas_jugador("Jugador23", tablero);
-        pares_jugador1 += son_pares(1,tablero, escondidas,r1,c1,r2,c2)
-        print("Pares Jugador23:",pares_jugador1)
-    elif caso == 4:
+    
+    if caso == 4:
         escondidas = [['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q'], ['a', 'b', 'c', 'd', 'e', 'f'], ['g', 'h', 'i', 'j', 'k', 'l'], ['m', 'n', 'ñ', 'o', 'p', 'q']]
         tablero = [['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?'], ['?', '?', '?', '?', '?', '?']]
         r1,c1,r2,c2 = leer_cartas_jugador("Jugador1", tablero);
